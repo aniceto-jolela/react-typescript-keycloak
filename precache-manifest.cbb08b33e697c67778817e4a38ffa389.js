8ea85ae36fdd3daa216ded54823db30e207ff7cf
aniceto-jolela <anicetojosejolelaj@gmail.com>
2022-12-22 11:36:31 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.cbb08b33e697c67778817e4a38ffa389.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7cb701c97383558fb88",
+    "url": "/react-typescript-keycloak/static/js/main.b7cb701c.chunk.js"
+  },
+  {
+    "revision": "d0e0a002cd9b1f471f92",
+    "url": "/react-typescript-keycloak/static/js/1.d0e0a002.chunk.js"
+  },
+  {
+    "revision": "1e4b2e594634b2f8d808",
+    "url": "/react-typescript-keycloak/static/js/runtime~main.1e4b2e59.js"
+  },
+  {
+    "revision": "aacb5b9db515f8e2524d0fe50e1e0ee8",
+    "url": "/react-typescript-keycloak/index.html"
+  }
+];
